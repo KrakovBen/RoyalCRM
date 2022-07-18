b409a2c2f307b9ca1dc0c5b6034da238d498a0b4
KrakovBen <93730442+KrakovBen@users.noreply.github.com>
2022-07-18 18:29:23 +0300

    root

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './routes/app-routing.module';
-import { AppComponent } from './layout/app.component';
+import { AppComponent } from './root/app.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
