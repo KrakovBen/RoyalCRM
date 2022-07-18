6f832fd05e2d3a72fb7e515c0a068101f85fa610
KrakovBen <93730442+KrakovBen@users.noreply.github.com>
2022-07-18 18:23:00 +0300

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './routes/app-routing.module';
 import { AppComponent } from './layout/app.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { MainComponent } from './layout/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
