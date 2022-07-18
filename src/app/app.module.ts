cdf6f1ed3fdbe363c3a912580de69f92a6e6739c
KrakovBen <93730442+KrakovBen@users.noreply.github.com>
2022-07-18 19:52:57 +0300

    class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,12 @@ import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { MainComponent } from './layout/main/main.component';
 import { SideNavComponent } from './layout/main/side-nav/side-nav.component';
+import { LogoComponent } from './layout/header/logo/logo.component';
+import { HamburgerComponent } from './layout/header/hamburger/hamburger.component';
+import { RightMenuComponent } from './layout/header/right-menu/right-menu.component';
+import { LeftMenuComponent } from './layout/header/left-menu/left-menu.component';
+import { LoggedInComponent } from './layout/header/right-menu/logged-in/logged-in.component';
+import { LoginComponent } from './layout/header/right-menu/login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +22,13 @@ import { SideNavComponent } from './layout/main/side-nav/side-nav.component';
     HeaderComponent,
     FooterComponent,
     MainComponent,
-    SideNavComponent
+    SideNavComponent,
+    LogoComponent,
+    HamburgerComponent,
+    RightMenuComponent,
+    LeftMenuComponent,
+    LoggedInComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
